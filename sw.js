78e879a9a41621c5b19077297953c89659482159
Önder Sümer <ondersumer07@gmail.com>
2020-03-21 14:02:01 +0300

    PWA on IOS does not work, we are trying to solve it.

--- sw.js
+++ sw.js
@@ -18,43 +18,43 @@ self.addEventListener('install', event => {
 });
 
 // LOADING FILES WHEN OFFLINE//
-self.addEventListener('fetch', event => {
-  console.log('Fetch event for ', event.request.url);
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => {
-      if (response) {
-        console.log('Found ', event.request.url, ' in cache');
-        return response;
-      }
-      console.log('Network request for ', event.request.url);
-      return fetch(event.request)
-
-        .then(response => {
-          // TODO 5 - Respond with custom 404 page
-          return caches.open(staticCacheName).then(cache => {
-            cache.put(event.request.url, response.clone());
-            return response;
-          });
-        });
-
-
-    }).catch(error => {
-      console.log('Error, ', error);
-      return caches.match('./offline.html');
-    })
-  );
-});
-
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(async function () {
-//     try {
-//       return await fetch(event.request);
-//     } catch (err) {
-//       return caches.match(event.request);
-//     }
-//   }());
+// self.addEventListener('fetch', event => {
+//   console.log('Fetch event for ', event.request.url);
+//   event.respondWith(
+//     caches.match(event.request)
+//     .then(response => {
+//       if (response) {
+//         console.log('Found ', event.request.url, ' in cache');
+//         return response;
+//       }
+//       console.log('Network request for ', event.request.url);
+//       return fetch(event.request)
+//
+//         .then(response => {
+//           // TODO 5 - Respond with custom 404 page
+//           return caches.open(staticCacheName).then(cache => {
+//             cache.put(event.request.url, response.clone());
+//             return response;
+//           });
+//         });
+//
+//
+//     }).catch(error => {
+//       console.log('Error, ', error);
+//       return caches.match('./offline.html');
+//     })
+//   );
 // });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(async function () {
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
+    }
+  }());
+});
 
 // TODO 6 - Respond with custom offline page
 
